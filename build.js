c96efe5e817e22e4bf09cfe1c0be8d2f9f58d1b5
Jordi Izquierdo <hi@izquiratops.dev>
2022-06-27 11:14:13 +0200

    Red cube goes brrrr

---
+++ build.js
@@ -0,0 +1,34 @@
+const esbuild = require('esbuild');
+const fs = require('fs');
+const path = require("path");
+
+const copyRecursiveSync = (src, dest) => {
+    // Check if exists and it's a directory
+    const isDirectory = fs.existsSync(src) && fs.statSync(src).isDirectory();
+
+    // If: it's a directory then we look inside
+    // Else: copy file into 'dest'
+    if (isDirectory) {
+        // Create dest folder if it's not already done
+        if (!fs.existsSync(dest)) { fs.mkdirSync(dest); }
+
+        // Look for every item inside for recursive folders
+        fs.readdirSync(src)
+            .filter(file => !file.match(/.js|.jsx|.ts|.tsx/))
+            .forEach((dir) => {
+                const childSrc = path.join(src, dir);
+                const childDest = path.join(dest, dir);
+                copyRecursiveSync(childSrc, childDest);
+            })
+    } else {
+        fs.copyFileSync(src, dest);
+    }
+}
+
+esbuild.build({
+        entryPoints: ['src/main.js'],
+        bundle: true,
+        outdir: 'dist'
+    })
+    .then(_ => copyRecursiveSync('./src', './dist'))
+    .catch(() => process.exit(1));
